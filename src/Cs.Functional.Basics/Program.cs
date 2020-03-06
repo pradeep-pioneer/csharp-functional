ed6200169ac889768ea5e078906d3cc6254a672e
Pradeep Singh <pradeep@surfer-172-20-7-249-hotspot.surfsonix.com>
2020-03-06 13:26:11 +0400

    added project for basics

---
+++ src/Cs.Functional.Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs.Functional.Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
